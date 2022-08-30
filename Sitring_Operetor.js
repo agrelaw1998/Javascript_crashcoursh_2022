492e958875fe8d51a4ade4e75b87e6ef7022990c
agrelaw1998 <112468952+agrelaw1998@users.noreply.github.com>
2022-08-30 13:28:42 +0530

    Sitring operator

---
+++ Sitring_Operetor.js
@@ -0,0 +1,10 @@
+//----------------------Sitring Operetor-----------------
+// The concatenation operator(+) concatenates two string values together,
+// returning another string that is the union of the two operand strings,
+
+// console.log("Hello world");
+// console.log("hello " + "world");
+
+var myName= "Pranay";
+console.log(myName + "Agre");
+console.log(myName + "Omprakash Agre");
---
+++ index.js
@@ -0,0 +1,140 @@
+//console.log("Hello lol");
+//var _MyName='Prany Omprakasha Agre';
+//var _1my__Age= '24';
+//console.log(_MyName);
+// console.log(_1my__Age);
+
+                        //***** Section 4 data type
+// var Name= 'Raj'
+// console.log(Name);
+
+// var Age= 24;
+// console.log(Age);
+
+// var iAmPranay = false;
+// console.log(iAmPranay);
+
+// type of operator
+//console.log(typeof(iAmPranay));
+
+
+                              //**2 Callenge Time
+//console.log('10'+'20'); 
+// console.log('20'-'10');  
+// console.log("java"+"Script");
+
+//console.log(" "+ "0");
+//'pranay'-'Harish'
+// console.log('pranay'-'Harish');
+
+//True + True
+//True - False
+//ture + false
+//False - True
+
+// console.log(true + true);
+// console.log(true - false );
+// console.log(ture + false);
+// console.log('False' - 'True');
+
+
+//  var imuseless = null
+// console.log(imuseless);
+// console.log(typeof(imuseless));
+
+// var iAmPranay;
+//console.log(ture + false);
+
+// var imuseless;
+// console.log(imuseless);
+// console.log(typeof(imuseless));
+
+// --------------------------------------------------------------------
+// var MyPhoneNumber = 8446966707;
+// var _MyName = "Radheram";
+
+// console.log(isNaN(MyPhoneNumber));
+// console.log(isNaN(_MyName));
+
+// if(isNaN(_MyName)){
+    // console.log("Duplicate Number found");
+
+// }
+
+// --------------------------------------------------------------------------
+
+
+
+// NaN === NaN
+// Number.NaN === NaN;
+// isNaN(NaN);
+// isNaN(Number.NaN);
+// Number.isNaN(NaN);
+
+// console.log(NaN === NaN);
+// console.log(Number.NaN === NaN);
+// console.log(isNaN(NaN));
+// console.log(Number.isNaN(NaN));
+
+
+// --------------Assignmant Opertor-----------------
+
+// var x = 5;
+// var y = 5;
+
+// console.log("is the both x and y are euqal" + x = y);
+
+
+// ----------------Arithmethic Opertor-----------------
+//An arithmethinc Opertor takes numerical value
+
+// console.log(3+3);
+// console.log(10-5);
+// console.log(40/4);
+// console.log(5*5);
+
+// console.log("Remainder Opertor" + 40%2);
+
+
+
+// Increment and Decrement
+// var num = 15;
+// var newNum = num++  + 5;  
+// console.log(num);
+// console.log(newNum);
+
+
+// var num = 15;
+// var newNum = --num + 5;
+// console.log(num);
+// console.log(newNum);
+
+// ----------------- logical Operetor --------------------
+
+var a=19;
+var b=-15;
+// console.log(a<b);
+// console.log(a>b && b>0 && b>0);
+
+// ----logical or (&&)-------
+// console.log(a>b && b>0 -20 && b>0);
+
+
+// -----logical or (||)------
+// console.log((a>b || b>0 -20 || b>0));
+
+//-------logical or (!)--------
+// console.log(!false);
+// console.log(!((a>b) || (b>0)));
+
+
+//----------------------Sitring Operetor-----------------
+// The concatenation operator(+) concatenates two string values together,
+// returning another string that is the union of the two operand strings,
+
+// console.log("Hello world");
+// console.log("hello " + "world");
+
+var myName= "Pranay";
+console.log(myName + "Agre");
+console.log(myName + "Omprakash Agre");
